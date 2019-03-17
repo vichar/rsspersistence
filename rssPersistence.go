69b9e01dfbcc080e5eda18599d11696146d8dc4d
Vichar Nuchsiri 52545 <vichar.nuchsiri@fico.com>
2019-03-17 19:18:51 +0700

    yaml parser

--- rssPersistence.go
+++ rssPersistence.go
@@ -2,10 +2,25 @@ package rsspersistence
 
 import (
 	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
 )
 
 // ReadTextFile takes path and returns content of file
 func ReadTextFile(path string) (string, error) {
 	buffer, error := ioutil.ReadFile(path)
 	return string(buffer), error
+}
+
+// ParseContent parsing content of a YML files
+func ParseContent(content string) (FirebaseDatabaseConnnection, error) {
+	connection := FirebaseDatabaseConnnection{}
+	error := yaml.Unmarshal([]byte(content), &connection)
+	return connection, error
+}
+
+// FirebaseDatabaseConnnection contains data for connecting to Fireware Database
+type FirebaseDatabaseConnnection struct {
+	Key   string
+	Topic string
 }
--- rssPersistence_test.go
+++ rssPersistence_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/vichar/rsspersistence"
 )
 
-func TestParseDatabaseConfiguration(t *testing.T) {
+func TestReadingConfiguration(t *testing.T) {
 
 	t.Run("ReadingTextFile Happy Path", func(t *testing.T) {
 		content, error := rsspersistence.ReadTextFile("./test.txt")
@@ -35,6 +35,26 @@ func TestParseDatabaseConfiguration(t *testing.T) {
 			get := strings.TrimSpace(content)
 			want := "open : no such file or directory"
 			t.Errorf("Expected to get %s but get %s", want, get)
+		}
+	})
+
+	t.Run("ParsingConfiguration File to Object", func(t *testing.T) {
+		data := `
+        key: 0c681e1e-48ad-11e9-8646-d663bd873d93
+        topic: /topics/test
+        `
+		content, error := rsspersistence.ParseContent(data)
+		if error != nil {
+			get := error.Error()
+			t.Errorf("It should not return any errors %s", get)
+		} else {
+
+			if "0c681e1e-48ad-11e9-8646-d663bd873d93" != content.Key {
+				t.Errorf("It should contain assigned key: 0c681e1e-48ad-11e9-8646-d663bd873d9")
+			}
+			if "/topics/test" != content.Topic {
+				t.Errorf("It should contain assigned topic: /topics/test")
+			}
 		}
 	})
 }
